081606cf0c93fd446819afea22a6199a2d90b28b
Sabrina Inzilloemailpetitcomiteparis@gmail.com <petitcomiteparis@gmail.com>
2022-10-25 15:50:46 +0200

    Initial Commit

---
+++ 3DcardErsatz/_DcardErsatzApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DcardErsatzApp.swift
+//  3DcardErsatz
+//
+//  Created by Sabrina Inzillo on 25.10.22.
+//
+
+import SwiftUI
+
+@main
+struct _DcardErsatzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
